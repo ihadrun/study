f5b109cd16c543ac7cb74939962d4e382dad3de9
ihadrun <anirudhoffline@gmail.com>
2022-03-24 11:05:41 +0530

    biginteger

---
+++ Hackerrank-Java-BigInteger/src/Solution.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.math.BigInteger;
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+         Scanner sc= new Scanner(System.in);
+         BigInteger A = sc.nextBigInteger();
+         BigInteger B= sc.nextBigInteger();
+         System.out.println(A.add(B));
+         System.out.println(A.multiply(B));
+         
+         
+         
+         
+    }
+}
--- Hackerrank-java-BigDecimal/src/Solution.java
+++ Hackerrank-java-BigDecimal/src/Solution.java
@@ -5,7 +5,7 @@ class Solution{
     public static void main(String []args){
         //Input
         Scanner sc= new Scanner(System.in);
-        int n=sc.nextInt();
+       int n=sc.nextInt(); 
         String []s=new String[n+2];
         for(int i=0;i<n;i++){
             s[i]=sc.next();
