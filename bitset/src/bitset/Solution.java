e4e9052f918231b774d56b3d4d35871fcac23c3f
ihadrun <anirudhoffline@gmail.com>
2022-09-22 12:15:17 +0530

    bitset

---
+++ bitset/src/bitset/Solution.java
@@ -0,0 +1,27 @@
+package bitset;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner(System.in);
+    	int N = sc.nextInt();
+    	  BitSet B1 = new BitSet(N);
+          BitSet B2 = new BitSet(N);
+          for(int i=0;i<N; i++) {
+        	  B1.set(0);
+        	  
+        	  
+          }
+          System.out.println("bs1  : " + B1);
+          System.out.println("bs2  : " + B2);
+    	
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
