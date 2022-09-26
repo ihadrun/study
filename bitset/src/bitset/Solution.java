b2e4b065aa58f5020328cc63aa4293a918918b7e
ihadrun <anirudhoffline@gmail.com>
2022-09-26 23:56:01 +0530

    stiltry

--- bitset/src/bitset/Solution.java
+++ bitset/src/bitset/Solution.java
@@ -12,9 +12,25 @@ public class Solution {
     	
     	Scanner sc = new Scanner(System.in);
     	int N = sc.nextInt();
+    	int M = sc.nextInt();
     	  BitSet B1 = new BitSet(N);
           BitSet B2 = new BitSet(N);
-          for(int i=0;i<N; i++) {
+         String s = sc.next();
+          for(int i=0;i<M; i++) {
+        	  switch(s) {
+        	  case "AND":
+        		  B1.and(B2);
+        	    // code block
+        	    break;
+        	  case "OR":
+        		  B1.or(B2);
+        	  case "FLIP":
+        		  B1.flip(bitIndex);
+        	    // code block
+        	    break;
+        	  default:
+        	    // code block
+        	}
               B1.set(i);
         }
           System.out.println("bs1  : " + B1);
