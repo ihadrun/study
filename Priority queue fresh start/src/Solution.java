ef86fb4b799b7421a818233dec6e347caf6e3827
ihadrun <anirudhoffline@gmail.com>
2022-10-10 11:30:49 +0530

    try1

--- Priority queue fresh start/src/Solution.java
+++ Priority queue fresh start/src/Solution.java
@@ -1,4 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.HashSet;
 
 public class Solution {
 
-}
+ public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+System.out.println(t);
+        }
+        
+        
+
+
+//Write your code here
+
+    }
